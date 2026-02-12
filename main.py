fd85634a77e18f4dd5070423378ff514d8f6a32a
LuanAllves <luancristianrodriguesalves@gmail.com>
2026-02-12 00:37:55 -0300

    feat: add official branding, app icon and splash screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-from PySide6.QtWidgets import QApplication
+import sys, os
+from PySide6.QtWidgets import QApplication, QSplashScreen
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import Qt, QTimer
 from gui.main_window import MainWindow
 import ctypes
 import platform
@@ -10,6 +12,14 @@ if platform.system() == "Windows":
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    
+    pixmap = QPixmap(os.path.join("gui", "assets", "splash.png"))
+    splash = QSplashScreen(pixmap, Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+    splash.setAttribute(Qt.WA_TranslucentBackground)
+    splash.show()
+    splash.showMessage("Iniciando Sistema WL Signage...", Qt.AlignBottom | Qt.AlignCenter, Qt.white)
+
     window = MainWindow()
-    window.show()
+    
+    QTimer.singleShot(2500, lambda: (window.show(), splash.finish(window)))
     sys.exit(app.exec())
